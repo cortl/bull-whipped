e1c84a85356e5287f30d0ed6c1779e15c98b29b8
Cortlan Bainbridge <bainbric@uni.edu>
2017-03-13 08:20:07 -0500

    more work on getting the site to display information as we would want to see it.  Next thing is to make the chart live update and only pull the last some number of entries in the database

--- index.js
+++ index.js
@@ -22,6 +22,15 @@ app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
   res.render('pages/index');
 });
+
+app.get('/basement',function(req, res) {
+  res.render('pages/basement');
+});
+
+app.get('/livingroom',function(req, res) {
+  res.render('pages/basement');
+});
+
 app.get('/record', function(req, res){
   res.render('pages/record');
 });
@@ -31,7 +40,9 @@ app.post('/record', function(req, res) {
   var sensor = req.body.sensor;
   var measurement = req.body.data;
 
-  if (sensor == "" || measurement == ""){
+  console.log(sensor);
+  console.log(measurement);
+  if (sensor == "" || measurement == "" || typeof sensor == 'undefined' || typeof measurement == 'undefined'){
     // Nothing was sent, send 400 bad request
     res.writeHead(400, {'Content-Type': 'text/html'});
     res.end("<!doctype html><html><head><title>Bad Request</title></head><body>Bad Request.</body></html>")
