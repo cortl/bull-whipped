2eeecea9c91d7bc23c0ab8677d9dda7ddaa8e90d
Cortlan Bainbridge <bainbric@uni.edu>
2017-04-21 08:16:56 -0500

    Website hooked up to an RF sensor that reads in from the serial port COM5 on windows.  Also removed the post data route since we wont be using it. Removed the helper.js since it's not doing anything.

--- db.js
+++ db.js
@@ -9,10 +9,10 @@ module.exports = function(db, app) {
         db.run("CREATE TABLE avail_sensors (id INTEGER PRIMARY KEY AUTOINCREMENT, sensorname TEXT, title TEXT, measurement TEXT, type TEXT REFERENCES sensor_types(type))")
         db.run("CREATE TABLE measurements (sensorID INTEGER, measurementsID INTEGER PRIMARY KEY AUTOINCREMENT, date TEXT, value FLOAT, FOREIGN KEY (sensorID) REFERENCES avail_sensors(id))")
         // Fill database with test information
-        sensorCreate(db, "temp1", "Internal Temperature", "Fahrenheit (F)", 1, "Thermometer");
-        sensorCreate(db, "sump", "Sump Pump", "Distance (cm)", 2, "Water");
-        sensorCreate(db, "temp2", "Outside Temperature", "Fahrenheit (F)", 3, "Thermometer");
-        sensorCreate(db, "remote", "TV Remote", "", 4, "Remote");
+        // sensorCreate(db, "temp1", "Internal Temperature", "Fahrenheit (F)", 1, "Thermometer");
+        // sensorCreate(db, "sump", "Sump Pump", "Distance (cm)", 2, "Water");
+        // sensorCreate(db, "temp2", "Outside Temperature", "Fahrenheit (F)", 3, "Thermometer");
+        // sensorCreate(db, "remote", "TV Remote", "", 4, "Remote");
     });
 }
 
--- routes.js
+++ routes.js
@@ -51,67 +51,7 @@ module.exports = function(db, app) {
         });
     });
 
-    // Get Posted Information and save to database
-    // app.post('/record', function(req, res) {
-    //     var sensor = req.body.sensor;
-    //     var value = req.body.data;
-    //     var title = req.body.title;
-    //     var measurement = req.body.measurement;
-    //     // font-awesome icons (for customization)
-    //     var pageicon = req.body.icon;
-    //     if (sensor == "" || value == "" || typeof sensor == 'undefined' || typeof value == 'undefined') {
-    //         // Nothing was sent, send 400 bad request
-    //         res.writeHead(400, {
-    //             'Content-Type': 'text/html'
-    //         });
-    //         res.end("<!doctype html><html><head><title>Bad Request</title></head><body>Bad Request.</body></html>")
-    //     } else {
-    //         // Check if table exists for that sensor
-    //         db.all("SELECT count(*) AS rowCount FROM avail_sensors WHERE sensorname='" + sensor + "';", function(err, row) {
-    //             if (row[0].rowCount == 0) {
-    //                 // sensor doesn't exist, create it.
-    //                 console.log("Attempting to register a new sensor: " + sensor + "...");
-    //                 // need measurement and title variables inputted to create a new table, if their aren't any then tell them off
-    //                 if (measurement == "" || typeof measurement == 'undefined' || title == "" || typeof title == 'undefined') {
-    //                     console.log("Sensor didn't provide enough information to properly register, ignoring request.")
-    //                     res.writeHead(400, {
-    //                         'Content-Type': 'text/html'
-    //                     });
-    //                     res.end("<!doctype html><html><head><title>Bad Request</title></head><body>Data doesn't exist in database, no measurment or title sent either.  Unable to create sensor data.</body></html>")
-    //                 } else {
-    //                     console.log("Sensor has been registered.")
-    //                     if (typeof pageicon != 'undefined' || pageicon != "") {
-    //                         var state = 'INSERT INTO avail_sensors(sensorname, title, measurement, pageicon) VALUES ("' + sensor + '", "' + title + '", "' + measurement + '", "' + pageicon + '");';
-    //                     } else {
-    //                         var state = 'INSERT INTO avail_sensors(sensorname, title, measurement) VALUES ("' + sensor + '", "' + title + '", "' + measurement + '");';
-    //                     }
-    //                     // console.log(state)
-    //                     db.run(state);
-    //                     res.writeHead(200, {
-    //                         'Content-Type': 'text/html'
-    //                     });
-    //                     res.end("<!doctype html><html><head><title>Received</title></head><body>Data Received.</body></html>")
-    //                     updateRoutes()
-    //                 }
-    //             } else {
-    //                 // sensor has been registered, add the measurement in.
-    //                 db.all("SELECT * FROM avail_sensors WHERE sensorname = \"" + sensor + "\"", function(err, row) {
-    //                     sensorID = row[0].id;
-    //                     var query = 'INSERT INTO measurements(sensorID, date, value) VALUES (' + sensorID + ', CURRENT_TIMESTAMP, ' + value + ');'
-    //                     var stmt = db.prepare(query);
-    //                     stmt.run()
-    //                     stmt.finalize()
-    //                     res.writeHead(200, {
-    //                         'Content-Type': 'text/html'
-    //                     });
-    //                     res.end("<!doctype html><html><head><title>Received</title></head><body>Data Received.</body></html>")
-    //                     console.log("Recorded Data from Sensor: " + sensor + ", Measurement: " + value)
-    //                 })
-    //             }
-    //         });
-    //     }
-    // });
-    app.get('/getData', function(req, res) {
+	app.get('/getData', function(req, res) {
         var sensor = req.query.sensor
         var last = req.query.last
         if (sensor != "" && typeof sensor != 'undefined') {
